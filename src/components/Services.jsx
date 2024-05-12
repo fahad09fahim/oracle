513679d0def8814d7d6f493e96c8b6722977b84d
Fahim <anik.fahim07@gmail.com>
2024-05-12 21:08:31 +0600

    services second card completed

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,13 @@
-import { check, service1 } from "../assets";
-import { oracleServices, oracleServicesIcons } from "../constants";
+import { check, service1, service2 } from "../assets";
+import { oracleServices } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoBar,
+  VideoChatMessage,
+} from "./design/Services";
 
 const Services = () => {
   return (
@@ -24,15 +30,54 @@ const Services = () => {
             </div>
             <div className="relative z-1 ml-auto max-w-[17rem] ">
               <h2 className="h2 mb-4">Smartest AI</h2>
-              <p className="body-2 mb-[3rem] text-n-2/9">The Oracle unlocks the potential of AI-powered applications</p>
+              <p className="body-2 mb-[3rem] text-n-2/9">
+                The Oracle unlocks the potential of AI-powered applications
+              </p>
               <ul className="body-2">
                 {oracleServices.map((item, index) => (
-                  <li key={index} className="flex items-end py-4 border-t border-n-4 gap-[1.4rem]">
+                  <li
+                    key={index}
+                    className="flex items-end py-4 border-t border-n-4 gap-[1.4rem]"
+                  >
                     <img src={check} alt="" />
                     <p>{item}</p>
                   </li>
                 ))}
               </ul>
+            </div>
+          </div>
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+            <div className="relative border border-n-1/10 min-h-[39rem] overflow-hidden rounded-3xl ">
+              {/* insert image into the div */}
+              <div className="absolute inset-0">
+                <img
+                  src={service2}
+                  className="w-full h-full object-cover"
+                  width={630}
+                  height={750}
+                  alt=""
+                />
+              </div>
+              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
+                <h2 className="h2 mb-4">Photo editing</h2>
+                <p className="body-2 mb-[3rem] text-n-2/9">
+                  Automatically enhance your photos using our AI app&apos;s
+                  photo editing feature. Try it now!
+                </p>
+              </div>
+              <PhotoChatMessage />
+            </div>
+            <div className="p-4 overflow-hidden rounded-3xl lg:min-h-[46rem] bg-n-7">
+              <div className="py-12 px-4 xl:px-8">
+                <h2 className="h2 mb-4">Video generation</h2>
+                <p className="body-2 mb-[2rem] text-n-3">
+                  The world&’s most powerful AI photo and video art generation
+                  engine. What will you create?
+                </p>
+                <ul className="flex">
+                    
+                </ul>
+              </div>
             </div>
           </div>
         </div>
